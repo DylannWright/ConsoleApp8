3aad67293d53c7bfcfae5ba6c72f6514fdea94fb
Dylan Wright <17WrighD@fulford.york.sch.uk>
2022-11-10 12:34:39 +0000

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rocket Sputnick = new Rocket();
+            Console.WriteLine (Sputnick.Countdown(10));
+
+        }
+    }
+}
---
+++ ConsoleApp8/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Rocket
+    {
+        public string name = "Sputnick";
+        public string destination = "Mars";
+
+        internal string Countdown(int time)
+        {
+            string pMessage = "";
+
+
+            if (time >= 0)
+                {
+                for (int i = time; i > 0; i++)
+                {
+                    pMessage += Convert.ToString(i);
+                    pMessage += ",";
+                }
+                pMessage += $"Blast off! {name} is going to {destination}!";
+            }
+            else
+            {
+                pMessage = $"{name} launch to {destination} cancelled due to invalid countdown";
+            }
+            return pMessage;
+        }
+    }
+}
